1d6a35b2b8cff8b728435d9407f44aea6ec4e408
Sakamoto0608 <kbc20f02@stu.kawahara.ac.jp>
2021-09-02 11:51:45 +0900

    fix:ヘッダーの修正
    ブランド名を変更し、無駄なリンクをそぎ落としました。

--- function.php
+++ function.php
@@ -3,20 +3,14 @@ function headerGenerate(){
 $header = <<<EOD
 <div class="container-fluid bg-dark">
 <nav class="navbar navbar-expand-lg navbar-dark">
-<a class="navbar-brand" href="index.php">ブランド名の予定</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#切り替え表示されるコンテンツ名" aria-controls="切り替え表示されるコンテンツ名" aria-expanded="false" aria-label="ナビゲーション切替">
+<a class="navbar-brand" href="index.php">SAKAMOSUITURE</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#content" aria-controls="content" aria-expanded="false" aria-label="ナビゲーション切替">
 <span class="navbar-toggler-icon"></span>
 </button>
-<div class="collapse navbar-collapse" id="切り替え表示されるコンテンツ名">
+<div class="collapse navbar-collapse" id="content">
 <ul class="navbar-nav mr-auto">
 <li class="nav-item active">
 <a class="nav-link" href="tweet.php">投稿する</a>
-</li>
-<li class="nav-item active">
-<a class="nav-link" href="#">何かのリンク</a>
-</li>
-<li class="nav-item active">
-<a class="nav-link" href="#">何かのリンク</a>
 </li>
 </ul>
 <form class="form-inline my-2 my-lg-0">
